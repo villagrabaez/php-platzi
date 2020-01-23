adbad004af823aa920ee93cf3b55f6b598ae161b
Bernardino Villagra <bernardinovillagra@hotmail.com>
2020-01-23 11:08:56 -0300

    Cambiando configuracion htaccess para heroku

--- public/index.php
+++ public/index.php
@@ -7,6 +7,11 @@ error_reporting('E_ALL');
 
 require_once '../vendor/autoload.php';
 
+// PHPDOTENV
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
+
 // Eloquent
 
 use Illuminate\Database\Capsule\Manager as Capsule;
@@ -16,14 +21,14 @@ use App\Models\Job;
 $capsule = new Capsule;
 
 $capsule->addConnection([
-  'driver'    => 'mysql',
-  'host'      => 'localhost',
-  'database'  => 'php-platzi',
-  'username'  => 'root',
-  'password'  => '',
-  'charset'   => 'utf8',
-  'collation' => 'utf8_unicode_ci',
-  'prefix'    => '',
+  'driver'    => getenv('DB_CONNECTION'),
+  'host'      => getenv('DB_HOST'),
+  'database'  => getenv('DB_DATABASE'),
+  'username'  => getenv('DB_USERNAME'),
+  'password'  => getenv('DB_PASSWORD'),
+  'charset'   => getenv('DB_CHARSET'),
+  'collation' => getenv('DB_COLLATION'),
+  'prefix'    => getenv('DB_PREFIX'),
 ]);
 
 // Make this Capsule instance available globally via static methods... (optional)
